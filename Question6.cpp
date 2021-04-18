ebba134430b989019dbddf0c69580c9c2ee8fef1
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-04-18 20:47:05 +0530

    Add files via upload

--- Question6.cpp
+++ Question6.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	//code
-	int n;
-	cin>>n;
-	int a[n];
-	for(int i=0 ; i<n ; i++)
-	cin>>a[i];
-	int i = -1;
-	int pivot = 0;
-	for(int j=0 ; j<n ; j++)
-	{
-	    if(a[j]<pivot){
-	        
-	        i++;
-	    swap(a[i],a[j]);
-	        
-	    }
-	    
-	    
-	}
-	
-	for(int i=0 ; i<n ; i++)
-	cout<<a[i]<<" ";
-	return 0;
-	
+int main()
+{
+ int t;
+cin>>t;
+while(t--)
+{
+   int n,m;
+   cin>>n>>m;
+   int a[n], b[n];
+   unordered <int,int> map;
+   for(int i=0 ; i<n ; i++)
+   {
+          cin>>a[i];
+map[a[i]]++;
 }
+
+for(int j=0 ; j<m ; j++)
+   {
+          cin>>b[i];
+map[b[i]]++;
+}
+
+cout<<map.size()<<endl;
+}
+
